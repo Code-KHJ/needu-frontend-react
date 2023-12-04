fc4c2f9d94949434be04a7a210460bc71b344c8a
hyunjun <guswns212000@gmail.com>
2023-12-04 21:43:02 +0900

    create project

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import svgrPlugin from 'vite-plugin-svgr';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), svgrPlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
