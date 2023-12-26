50c31428facc5be4edfcc7e042e7a73d592edfba
hyunjun <guswns212000@gmail.com>
2023-12-26 22:10:12 +0900

    feat: cors 해결, api 테스트

--- src/api/user.tsx
+++ src/api/user.tsx
@@ -1,18 +1,24 @@
-import axios from "axios";
+import axios from 'axios';
 
 const userApi = {
   duplic: async (type: string, value: string) => {
-    if (type == "id") {
-      if (value == "abc@abc.com") {
-        return false;
+    const userDuplicDto = {
+      item: type,
+      value: value,
+    };
+    const response = await axios.post(
+      'http://localhost:8000/api/user/duplic',
+      userDuplicDto,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
       }
+    );
+    if (response.data) {
       return true;
-    }
-    if (type == "nickname") {
-      if (value == "nick") {
-        return false;
-      }
-      return true;
+    } else {
+      return false;
     }
   },
 };
