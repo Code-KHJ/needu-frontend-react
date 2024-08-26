491575dbaa0bb50b076f092348b7557c9f036390
DESKTOP-0BC1261\GAION <hjk@gaion.kr>
2024-08-26 13:57:53 +0900

    fix pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import arrow_right from "@/assets/images/arrow_right.png";
+import dbl_arrow_right from "@/assets/images/dbl_arrow_right.png";
 
 interface PaginationProps {
   currentPage: number;
-  totalPages: number | undefined;
+  totalPages: number;
   onPageChange: (pageNumber: number) => void;
 }
 
@@ -28,7 +29,10 @@ const Pagination: React.FC<PaginationProps> = ({
     };
   }, []);
 
-  if (totalPages === undefined || totalPages === 0) {
+  if (totalPages === undefined) {
+    return <div style={{ width: "100%" }}>조회중...</div>;
+  }
+  if (totalPages === 0) {
     return <div style={{ width: "100%" }}>검색 결과가 없습니다.</div>;
   }
 
@@ -36,11 +40,11 @@ const Pagination: React.FC<PaginationProps> = ({
     Math.floor((currentPage - 1) / pagesPerGroup) * pagesPerGroup + 1;
   const endPage = Math.min(startPage + pagesPerGroup - 1, totalPages);
   const isFirstGroup = startPage === 1;
-  const isLastGroup = endPage === totalPages;
+  const isLastGroup = endPage === totalPages || totalPages === undefined;
 
   const handlePreviousGroup = () => {
     if (!isFirstGroup) {
-      onPageChange(startPage - pagesPerGroup);
+      onPageChange(endPage - pagesPerGroup);
     }
   };
   const handleNextGroup = () => {
@@ -48,21 +52,44 @@ const Pagination: React.FC<PaginationProps> = ({
       onPageChange(startPage + pagesPerGroup);
     }
   };
+  const handleFirstGroup = () => {
+    if (!isFirstGroup) {
+      onPageChange(1);
+    }
+  };
+  const handleLastGroup = () => {
+    if (!isLastGroup) {
+      onPageChange(totalPages);
+    }
+  };
 
   return (
     <>
       {!isFirstGroup && (
-        <div onClick={handlePreviousGroup}>
-          <img
-            src={arrow_right}
-            style={{
-              width: "7px",
-              height: "14px",
-              transform: "rotate(180deg)",
-            }}
-            alt="left"
-          />
-        </div>
+        <>
+          <div onClick={handleFirstGroup}>
+            <img
+              src={dbl_arrow_right}
+              style={{
+                width: "16px",
+                height: "14px",
+                transform: "rotate(180deg)",
+              }}
+              alt="left"
+            />
+          </div>
+          <div onClick={handlePreviousGroup}>
+            <img
+              src={arrow_right}
+              style={{
+                width: "7px",
+                height: "14px",
+                transform: "rotate(180deg)",
+              }}
+              alt="left"
+            />
+          </div>
+        </>
       )}
       {Array.from({ length: endPage - startPage + 1 }, (_, index) => (
         <div
@@ -83,16 +110,28 @@ const Pagination: React.FC<PaginationProps> = ({
         </div>
       ))}
       {!isLastGroup && (
-        <div onClick={handleNextGroup}>
-          <img
-            src={arrow_right}
-            style={{
-              width: "7px",
-              height: "14px",
-            }}
-            alt="right"
-          />
-        </div>
+        <>
+          <div onClick={handleNextGroup}>
+            <img
+              src={arrow_right}
+              style={{
+                width: "7px",
+                height: "14px",
+              }}
+              alt="right"
+            />
+          </div>
+          <div onClick={handleLastGroup}>
+            <img
+              src={dbl_arrow_right}
+              style={{
+                width: "16px",
+                height: "14px",
+              }}
+              alt="right"
+            />
+          </div>
+        </>
       )}
     </>
   );
