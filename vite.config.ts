7ddea08cc6bb3a5892684e35d03b068eb2bab67d
Hyunjun Kim <guswns212000@gmail.com>
2024-10-16 21:43:36 +0900

    update: config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import react from "@vitejs/plugin-react-swc";
 import * as path from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [react(), svgrPlugin()],
   build: {
     outDir: "/app/needu/build",
+    // outDir: "./build",
   },
   resolve: {
     alias: [
