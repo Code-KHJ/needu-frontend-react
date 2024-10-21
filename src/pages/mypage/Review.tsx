58a2bae9b1d72c45f5fb03a6baa90f0fb5ae9a7d
DESKTOP-0BC1261\GAION <hjk@gaion.kr>
2024-10-21 16:56:46 +0900

    feat: mypage review

--- src/pages/mypage/Review.tsx
+++ src/pages/mypage/Review.tsx
@@ -1,8 +1,12 @@
 import reviewApi from "@/apis/review";
+import styles from "./Mypage.module.scss";
 import { useLoading } from "@/contexts/LoadingContext";
 import { ReviewContent, ReviewTrainingDto } from "@/interface/Review";
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import ico_like from "@/assets/images/ico_like.png";
+import ico_score from "@/assets/images/Star_1.png";
+import ico_arrow_down from "@/assets/images/ico_arrow_down.png";
 
 const Review = () => {
   const { showLoading, hideLoading } = useLoading();
@@ -11,7 +15,8 @@ const Review = () => {
   const navigate = useNavigate();
 
   const [reviewList, setReviewList] = useState<
-    ReviewContent[] | ReviewTrainingDto[]
+    | (ReviewContent & { expand: boolean })[]
+    | (ReviewTrainingDto & { expand: boolean })[]
   >([]);
 
   useEffect(() => {
@@ -29,7 +34,7 @@ const Review = () => {
       }
       const setData = response.data.map((item: any) => ({
         ...item,
-        status: false,
+        expand: false,
       }));
       setReviewList(setData);
     };
@@ -38,7 +43,83 @@ const Review = () => {
   }, [reviewType]);
   console.log(reviewList);
 
-  return <div>Review</div>;
+  return (
+    <div className={styles.review_wrap}>
+      <ul className={styles.review_list}>
+        {/* 접혔을떄 */}
+        {reviewList.map((review) => (
+          <li className={styles.review_item} key={review.id}>
+            <div className={styles.item_header}>
+              <h5 className={styles.corp_name}>
+                기관명입니다123123123기관명입니다123123123 기관명입니다123123123
+              </h5>
+              <div>점</div>
+            </div>
+            <div className={styles.highlight}>
+              한줄평입니다.123123123한줄평입니다.123123123
+              한줄평입니다.123123123 한줄평입니다.123123123
+            </div>
+            {review.expand ? (
+              <div className={styles.reaction_wrap}>
+                <div className={`body2 ${styles.date}`}>2024.10.10</div>
+                <div className={styles.reaction}>
+                  <span className={`body2`} style={{ color: "#aaa" }}>
+                    <img
+                      src={ico_like}
+                      alt="like"
+                      style={{ width: "16px", height: "16px" }}
+                    />
+                    10
+                  </span>
+                  <span className={`body2`} style={{ color: "#aaa" }}>
+                    <img
+                      src={ico_score}
+                      alt="score"
+                      style={{ width: "16px", height: "16px" }}
+                    />
+                    33333
+                  </span>
+                </div>
+              </div>
+            ) : (
+              <div className={styles.reaction_wrap}>
+                <div className={`body2 ${styles.date}`}>2024.10.10</div>
+                <div className={styles.reaction}>
+                  <span className={`body2`} style={{ color: "#aaa" }}>
+                    <img
+                      src={ico_like}
+                      alt="like"
+                      style={{ width: "16px", height: "16px" }}
+                    />
+                    10
+                  </span>
+                  <span className={`body2`} style={{ color: "#aaa" }}>
+                    <img
+                      src={ico_score}
+                      alt="score"
+                      style={{ width: "16px", height: "16px" }}
+                    />
+                    3
+                  </span>
+                </div>
+              </div>
+            )}
+
+            <button onClick={() => {}}>
+              <img
+                src={ico_arrow_down}
+                alt="더보기"
+                style={{
+                  width: "16px",
+                  transform: review.expand ? "rotate(180deg)" : "rotate(0deg)",
+                }}
+              />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Review;
