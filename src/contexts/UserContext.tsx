27d0c2ebb2143c3531cdd255b4d80e1c17b6506a
DESKTOP-0BC1261\GAION <hjk@gaion.kr>
2024-11-28 10:04:47 +0900

    fix user context type

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,18 +1,34 @@
 import userApi from "@/apis/user";
-import { createContext, useState, useContext, useEffect } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 import { useLoading } from "./LoadingContext";
 
-//@ts-ignore
-const UserContext = createContext();
-//@ts-ignore
-export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    id: null,
-    user_id: null,
-    nickname: null,
-    authority: null,
-    profile_image: null,
-  });
+type User = {
+  id: number | null;
+  user_id: string | null;
+  nickname: string | null;
+  authority: number | null;
+  profile_image: string | null;
+};
+type UserContextType = {
+  user: User;
+  setUser: React.Dispatch<React.SetStateAction<User>>;
+  loading: boolean;
+};
+
+const initialUserState: User = {
+  id: null,
+  user_id: null,
+  nickname: null,
+  authority: null,
+  profile_image: null,
+};
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<User>(initialUserState);
   const { showLoading, hideLoading } = useLoading();
   const [loading, setLoading] = useState(true);
   function getCookie(name: string) {
@@ -78,4 +94,10 @@ export const UserProvider = ({ children }) => {
   );
 };
 
-export const useUser = () => useContext(UserContext);
+export const useUser = (): UserContextType => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUser must be used within a UserProvider");
+  }
+  return context;
+};
